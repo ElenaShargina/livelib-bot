759c27fa2364ee3632d61c5bc8fd4c3a5ab54de0
user <elena.shargina@gmail.com>
2023-05-19 15:09:29 +0300

    Fixed bug with testing reader class

--- livelib/tests/test_reader.py
+++ livelib/tests/test_reader.py
@@ -251,6 +251,8 @@ class TestReader(CustomUnitTest):
         special_config.web_connection.cache_folder = 'data/sample/test_reader/get_read_books_from_page/cache'
         self.object = Reader(reader_name, WebWithCache(special_config), self.db_connection, self.export)
         # 2. Проверяем работу метода
+        print(len(self.object.get_read_books_from_page("/reader/Kasssiopei/read/~0")))
+        print(self.object.get_read_books_from_page("/reader/Kasssiopei/read/~0"))
         self.process_json_compare_to_json('get_read_books_from_page', 'get_read_books_from_page', 'output', 'input',
                                           False)
 
